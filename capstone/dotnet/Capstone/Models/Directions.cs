503f7d99d30a6bb11c99ab841212ca374ec81364
Daniel Suh <dsuhupcs@gmail.com>
2022-04-11 16:50:25 -0400

    Added Model Classes

---
+++ capstone/dotnet/Capstone/Models/Directions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Directions
+    {
+        public int StepsId { get; set; }
+        public string Description { get; set; }
+        public int StepsNumber { get; set; }
+        public int RecipeId { get; set; }
+    }
+}
