7e0015511f57af98a4fabc3c9f4f7b2072d86ede
Daniel Suh <dsuhupcs@gmail.com>
2022-04-21 16:55:19 -0400

    Updated meal plans

---
+++ capstone/vue/src/components/PlanRecipes.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      
+    </div>
+</template>
+
+
+<script>
+import recipesService from "@/services/RecipesService.js";
+
+export default {
+  name: "plan-recipes",
+  data(){
+    return {
+      searchBar:'',
+      ingredientList: [],
+      groceryList:[]
+    };
+  },
+  methods: {
+    addToGroceries(item){
+      this.groceryList.push(item);
+    }
+  },
+ created(){
+    recipesService.getAllIngredients().then(response => {
+          this.ingredientList = response.data;
+        });
+  },
+}
+</script>
